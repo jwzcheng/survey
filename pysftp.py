0004f3717d8295a2cd3600fb2174244096bce4c6
Jay Cheng <jwz.cheng@gmail.com>
2020-11-01 10:18:28 +0800

    del checkpoint

---
+++ pysftp.py
@@ -0,0 +1,24 @@
+import pysftp
+import pathlib
+
+sHostName = 'localhost'
+sUserName = 'jaycheng'
+sPassWord = '0000'
+
+cnopts = pysftp.CnOpts(knownhosts='my_macbook')
+cnopts.hostkeys = None
+
+with pysftp.Connection(sHostName, username=sUserName, password=sPassWord, cnopts=cnopts) as sftp:
+    # 移動目錄
+    sftp.cwd('./tmp/original/')
+
+    # 取得目錄內容
+    directory = sftp.listdir_attr()
+
+    # 印出結果
+    for attr in directory:
+        print(attr.filename, attr)
+
+# TEST copy files from /tmp/original
+original = '/Users/jaycheng/tmp/original'
+
